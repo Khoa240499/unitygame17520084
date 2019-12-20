df0d6ac3c85a74707add6b919769b6435d056714
Khoa240499 <44868690+Khoa240499@users.noreply.github.com>
2019-12-21 00:23:43 +0700

    update1

---
+++ test/Assets/Scripts/movingplat.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movingplat : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float speed = 0.05f, changeDirection = -1;
+    Vector3 Move;
+
+    public PauseUI pausep;
+
+    // Use this for initialization
+    void Start()
+    {
+        Move = this.transform.position;
+
+        pausep = GameObject.FindGameObjectWithTag("MainCamera").GetComponentInParent<PauseUI>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (pausep.pause)
+        {
+            this.transform.position = this.transform.position;
+
+        }
+        if (pausep.pause == false)
+        {
+            Move.x += speed;
+            this.transform.position = Move;
+        }
+
+
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.collider.CompareTag("ground"))
+        {
+            speed *= changeDirection;
+        }
+
+
+
+    }
+}
